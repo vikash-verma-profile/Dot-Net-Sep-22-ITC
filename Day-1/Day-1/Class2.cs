3a669d2ec36fb3aa921d791d3f0e7ffc2a4783ba
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-20 11:50:03 +0530

    added

--- Day-1/Day-1/Class2.cs
+++ Day-1/Day-1/Class2.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Collections;
 
 namespace Day_1
 {
+
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
     class Class2
     {
         public static void Main()
@@ -12,6 +19,32 @@ namespace Day_1
             IList<int> intList = new List<int>() { 10, 20, 30, 45, 50, 87 };
             var largest = intList.Max();
             Console.WriteLine("maximum number is "+largest);
+
+            //ofType
+
+            IList mixedList = new ArrayList();
+            mixedList.Add(0);
+            mixedList.Add("One");
+            mixedList.Add("Two");
+            mixedList.Add(3);
+            mixedList.Add(new Student() { StudentID=1,StudentName="Vikash"});
+
+            var stringResult = from s in mixedList.OfType<string>() select s;
+
+            foreach (var item in stringResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Output to get only integer values");
+            var intResult = from s in mixedList.OfType<int>() select s;
+
+
+            foreach (var item in intResult)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
