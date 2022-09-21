caba540658e3c8d34307037b42711d0f43e57178
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-21 12:59:45 +0530

    added code

---
+++ CustomerApp/CustomerApp/Models/CustomerDbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CustomerApp.Models
+{
+    public partial class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext()
+        {
+        }
+
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<TblDummyDatum> TblDummyData { get; set; }
+        public virtual DbSet<TblLogin> TblLogins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-PP0TB7N;Initial Catalog=CustomerDb;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.CustomerCode).HasMaxLength(200);
+
+                entity.Property(e => e.CustomerName).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<TblDummyDatum>(entity =>
+            {
+                entity.ToTable("tblDummyData");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.TextData).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("tblLogin");
+
+                entity.Property(e => e.Password).HasMaxLength(200);
+
+                entity.Property(e => e.UserName).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
