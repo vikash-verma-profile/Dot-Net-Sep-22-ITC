f10c6d5aa81c374612ba9b541880b3b222ae4e1a
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-22 09:25:11 +0530

    added code

---
+++ CustomerApp/CustomerApp/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using CustomerApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerDbContext db = new CustomerDbContext();
+        public string Get()
+        {
+            return "Vikash";
+        }
+        [Route("get-customers")]
+        public IEnumerable<Customer> GetCustomer()
+        {
+            return db.Customers;
+        }
+    }
+}
