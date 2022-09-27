03f8717fbafc9612c67b6e1178d45ceb93f65d0b
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-27 10:11:42 +0530

    added code

---
+++ WebApplication2/WebApplication2/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        SampleDBContext db = new SampleDBContext();
+
+        [HttpGet]
+        public IEnumerable<TblSample> Get()
+        {
+            return db.TblSamples;
+        }
+    }
+}
