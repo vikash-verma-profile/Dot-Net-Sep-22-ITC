5b41e72ca03c1d2d761a30318ce88432c7bf6f54
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-29 09:23:12 +0530

    added upload file

--- React-Labs/my-app/src/App.js
+++ React-Labs/my-app/src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const[picture,setPicture]=useState({});
+  const uploadPicture=(e)=>{
+    setPicture({
+      pictureAsFile:e.target.files[0]
+    });
+  };
   const setImageAction=async (event)=>{
     event.preventDefault();
-    const[pickture,SetPicture]
-    const uploadPicture=(e){
-
-    }
+    const formData=new FormData();
+    formData.append('file',picture.pictureAsFile);
     const data= await fetch("https://localhost:44374/api/Upload",{
     method:"post",
-    headers:{"Content-Type":"multipart/form-data"},
-    body:JSON.stringify({})
+    body:formData
     });
-    const UploadImage= await data.json();
+    const UploadImage= await data;
     console.log(UploadImage);
   }
   return (
