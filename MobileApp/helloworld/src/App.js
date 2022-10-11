4d695c8ff86ebc1221ac6f01f44b27bdf4cdc907
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-11 17:12:16 +0530

    added mobile app

--- MobileApp/helloworld/src/App.js
+++ MobileApp/helloworld/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { IonButton } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonButton,IonToast,useIonToast  } from '@ionic/react';
 
 function App() {
+  const [showToast,setShowToast]=useState(false);
+  const handleClick=()=>{
+    setShowToast(true);
+  }
   return (
     <div>
       <header>
         <h1>Myapp</h1>
       </header>
       <main>
-      <IonButton>Click Me</IonButton>
+      <IonButton onClick={handleClick}>Click Me</IonButton>
+      <IonToast isOpen={showToast} message="Hello I am Test" />
       </main>
     </div>
   );
