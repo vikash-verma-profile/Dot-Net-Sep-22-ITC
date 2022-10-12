5ce3d2da11d571a3e2cf47b2e83a0c17360dab96
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-12 18:07:37 +0530

    added

--- MobileApp/helloworld/src/hooks.js
+++ MobileApp/helloworld/src/hooks.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 export function useLocalStorage(key, defaultValue) {
-    const intialValue = localStorage.getItem(key) ?? defaultValue;
+    const getIntialValue =()=> localStorage.getItem(key) ?? defaultValue;
 
-    const [value, setValue] = useState(intialValue);
+    const [value, setValue] = useState(getIntialValue);
     const setAndStoreValue = (newValue) => {
         setValue(newValue)
         localStorage.setItem(key,newValue);
