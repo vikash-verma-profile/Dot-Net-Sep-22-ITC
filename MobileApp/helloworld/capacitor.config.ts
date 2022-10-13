68624a4bef1dadb67ae7f53067597a5b8629e2d3
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-13 16:29:57 +0530

    added code

---
+++ MobileApp/helloworld/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.helloworld',
+  appName: 'helloworld',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
