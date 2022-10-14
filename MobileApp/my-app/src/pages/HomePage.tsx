f8a441a107580a385fe26691bb3ae422fdc12e6c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-14 13:00:44 +0530

    added code

---
+++ MobileApp/my-app/src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
