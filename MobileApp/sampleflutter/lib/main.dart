c8ac395195e7bb3624f60e3f8e1b4872b0cb6995
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-10-18 11:34:42 +0530

    added code for sample app in flutter

--- MobileApp/sampleflutter/lib/main.dart
+++ MobileApp/sampleflutter/lib/main.dart
@@ -8,10 +8,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'what is your favorite color?',
+      'what is your favorite animal?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: const Text('Hello!'),
+      ),
+      body: Column(
+        children: [
+          const Text('The question'),
+          const ElevatedButton(child: const Text('Answer 1'), onPressed: null),
+          const ElevatedButton(child: const Text('Answer 2'), onPressed: null),
+          const ElevatedButton(child: const Text('Answer 3'), onPressed: null),
+        ],
       ),
     ));
   }
